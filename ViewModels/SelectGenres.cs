aa5de017416e4fea2e53b1b96cdf7e4af43251b3
mikistojanovski <92641504+mikistojanovski@users.noreply.github.com>
2022-05-26 20:41:52 +0200

    Add files via upload

---
+++ ViewModels/SelectGenres.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using soprosopro.Models;
+
+namespace soprosopro.ViewModels
+{
+    public class SelectGenres
+    {
+        public Movie Movie { get; set; }
+        public IEnumerable<int>? SelectedGenres { get; set; }
+        public IEnumerable<SelectListItem>? GenreList { get; set; }
+        public IEnumerable<int>? SelectedActors { get; set; }
+        public IEnumerable<SelectListItem>? ActorList { get; set; }
+        public IEnumerable<int>? SelectedProducers { get; set; }
+        public IEnumerable<SelectListItem>? ProducerList { get; set; }
+        public IEnumerable<int>? SelectedDirectors { get; set; }
+        public IEnumerable<SelectListItem>? DirectorList { get; set; }
+
+    }
+}
